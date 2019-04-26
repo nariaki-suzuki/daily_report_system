7d1e84c4e01939b40e034a5b6ba656d7fa0ea6d8
nariaki.suzuki <karurata@gmail.com>
2019-04-26 15:20:16 +0900

    Add Employee index

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 
 @NamedQueries({
     @NamedQuery(
-            name = "getAllEmplyees",
+            name = "getAllEmployees",
             query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
             ),
     @NamedQuery(
